7e9942a08f589c8b95ddf9cafb1882fc0cfd1db5
MinHyeJun <m2j9702@naver.com>
2017-02-21 16:26:48 +0900

    이진 탐색 트리 삭제 구현

--- Hyejun/Tree/Tree_Linkedlist.cpp
+++ Hyejun/Tree/Tree_Linkedlist.cpp
@@ -13,8 +13,9 @@ class myTree_L{
 	private:
 		Node * root;
 		Node * thisNode;
+		Node * parentNode;
 		int thisNodeNumber;
-	
+
 	public:
 		myTree_L()
 		{
@@ -55,14 +56,9 @@ class myTree_L{
 						thisNode -> rightNode = newNode;
 					return true;
 				}
-				else 
+				else
 					return false;
 			}
-		}
-
-		int getNodeNumber()
-		{
-			return thisNodeNumber;
 		}
 
 		Node * searchNode(Node * p, int data)
@@ -75,28 +71,184 @@ class myTree_L{
 			if(data < p -> data)
 			{
 				thisNodeNumber = thisNodeNumber * 2;
+				parentNode = thisNode;
 				return searchNode(p -> leftNode, data);
 			}
 			else if(data > p -> data)
 			{
 				thisNodeNumber = thisNodeNumber * 2 + 1;
+				parentNode = thisNode;
 				return searchNode(p -> rightNode, data);
 			}
 			else
 				return p;
+		}
+
+		int getNodeNumber()
+		{
+			return thisNodeNumber;
+		}
+
+		void deleteNode(int data)
+		{
+			parentNode = NULL;
+			thisNode = root;
+			Node * p = searchNode(thisNode, data);
+
+			cout << getNodeNumber() << endl;
+
+			if(p)
+			{
+				if(thisNode -> rightNode == NULL && thisNode -> leftNode == NULL)
+				{
+					if(parentNode)
+					{
+						if(parentNode -> leftNode -> data == thisNode -> data)
+							parentNode -> leftNode == NULL;
+						else
+							parentNode -> rightNode == NULL;
+					}
+
+					free(thisNode);
+				}
+				else if(thisNode -> rightNode == NULL)
+				{
+					if(parentNode)
+					{
+						if(parentNode -> leftNode -> data == thisNode -> data)
+							parentNode -> leftNode = thisNode -> leftNode;
+						else
+							parentNode -> rightNode = thisNode -> leftNode;
+					}
+					else
+						root = thisNode -> leftNode;
+
+					free(thisNode);
+				}
+				else if(thisNode -> leftNode == NULL)
+				{
+					if(parentNode)
+					{
+						if(parentNode -> leftNode -> data == thisNode -> data)
+							parentNode -> leftNode = thisNode -> rightNode;
+						else
+							parentNode -> rightNode = thisNode -> rightNode;
+					}
+					else
+						root = thisNode -> rightNode;
+
+					free(thisNode);
+				}
+				else
+				{
+					Node * leftDataNode = thisNode -> leftNode;
+					Node * rightDataNode = thisNode -> rightNode;
+					int leftlevel = 0, rightlevel = 0, temp;
+
+					while(leftDataNode -> rightNode != NULL)
+					{
+						leftDataNode = leftDataNode -> rightNode;
+						leftlevel++;
+					}
+
+					cout << "leftData: " << leftDataNode -> data << ", " << leftlevel << endl;
+
+					while(rightDataNode -> leftNode != NULL)
+					{
+						rightDataNode = rightDataNode -> leftNode;
+						rightlevel++;
+					}
+
+					cout << "rightData: " << rightDataNode -> data << ", " << rightlevel << endl;
+
+					if(leftlevel > rightlevel)
+					{
+						Node * originThisNode = thisNode;
+						Node * p = searchNode(root, leftDataNode -> data);
+						if(leftDataNode -> leftNode != NULL)
+						{
+							cout << "자식 노드 있음" << endl;
+							cout << "parentNodeData: " << parentNode -> data << endl;
+							temp = leftDataNode -> data;
+							leftDataNode -> data = leftDataNode -> leftNode -> data;
+							originThisNode -> data = temp;
+
+							//////////////////////////////////////////////////////////////////////////////////////// 딸린 자식이 계속 남아있음
+
+							free(leftDataNode -> leftNode);
+							leftDataNode -> leftNode = NULL;
+						}
+						else
+						{
+							cout << "자식노드 없음" << endl;
+							temp = leftDataNode -> data;
+							leftDataNode -> data = originThisNode -> data;
+							originThisNode -> data = temp;
+
+							parentNode -> rightNode = NULL;
+
+							free(leftDataNode);
+						}
+					}
+					else
+					{
+						Node * originThisNode = thisNode;
+						Node * p = searchNode(root, rightDataNode -> data);
+						if(rightDataNode -> rightNode != NULL)
+						{
+							cout << "자식노드 있음" << endl;
+							temp = rightDataNode -> data;
+							rightDataNode -> data = rightDataNode -> rightNode -> data;
+							originThisNode -> data = temp;
+
+							parentNode -> leftNode = rightDataNode -> rightNode;
+
+							free(rightDataNode -> rightNode);
+							rightDataNode -> rightNode = NULL;
+						}
+						else
+						{
+							cout << "자식노드 없음" << endl;
+							temp = rightDataNode -> data;
+							rightDataNode -> data = thisNode -> data;
+							originThisNode -> data = temp;
+
+							parentNode -> leftNode = NULL;
+
+							free(rightDataNode);
+						}
+					}
+				}
+			}
+			else
+				cout << "해당 데이터가 존재하지 않습니다." << endl;
 		}
 };
 
 int main(void)
 {
 	myTree_L t;
-	int num;
+	int menu, num;
 
 	while(1)
 	{
-		cin >> num;
-		t.insert(num);
-		cout << "NodeNumber : " << t.getNodeNumber() << endl;
+		cout << "1. insert   2. delete   :";
+		cin >> menu;
+
+		switch(menu)
+		{
+			case 1:
+				cin >> num;
+				t.insert(num);
+				cout << "NodeNumber : " << t.getNodeNumber() << endl;
+				break;
+			case 2:
+				cin >> num;
+				t.deleteNode(num);
+				break;
+			default:
+				exit(1);
+		}
 	}
 	return 0;
 }
